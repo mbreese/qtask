41f0bb80730949e5e6eb60476190b5882f0adca2
Marcus Breese <mbreese@stanford.edu>
2013-08-20 17:54:00 -0700

    0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='qtask',
-      version='0.1.0',
+      version='0.1.1',
       description='Utility library for submitting jobs to a cluster (SGE, PBS, etc)',
       author='Marcus Breese',
       author_email='mbreese@stanford.edu',
