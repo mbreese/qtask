8565c084971a76dca4bb9f2d8a12b77650e90e3e
Marcus Breese <mbreese@stanford.edu>
2013-08-26 13:24:21 -0700

    version bump, add monitor

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='qtask',
-      version='0.1.2',
+      version='0.1.3',
       description='Utility library for submitting jobs to a cluster (SGE, PBS, etc)',
       author='Marcus Breese',
       author_email='mbreese@stanford.edu',
       url='http://github.com/mbreese/qtask/',
-      packages=['qtask'],
+      packages=['qtask', 'qtask.monitor'],
       scripts=['bin/mqsub', 'bin/mqdel', 'bin/qtask-mon']
      )
